7251804f52e7695bd5a9227fdc300f6b47c96fb6
Michael Tamietti <michael@Michaels-MacBook-Pro.local>
2018-06-30 11:00:09 -0600

    added code to controller and routes

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,2 +1,21 @@
 class Api::ItemsController < ApplicationController
+
+  def index
+    render json: Item.all
+  end
+
+  def create
+    todo = Todo.create(:name, :price)
+    render json: todo
+  end
+
+  def update
+    todo = Todo.find(params[:id])
+    render json: todo
+  end
+
+  def destroy
+    Todo.find(params[:id]).destroy
+  end
+
 end
